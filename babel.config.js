72ff92a93e60e7430352cebb9dad6a4d3c8c126a
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-06-11 20:56:41 -0300

    in development

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ['@babel/preset-react'],
+  env: {
+    test: {
+      presets: ['es2015'],
+    },
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  target: 'node',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    libraryTarget: 'commonjs',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
+  externals: [
+    nodeExternals(),
+    'hyper/component',
+    'hyper/notify',
+    'hyper/decorate',
+    'react',
+    'electron',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
